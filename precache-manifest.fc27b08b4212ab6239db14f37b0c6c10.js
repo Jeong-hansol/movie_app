aa726970ccadf88ba805bfd8574a45b2e9358606
Jeong-hansol <jhs486674@gmail.com>
2020-08-30 15:25:08 +0900

    Updates

---
+++ precache-manifest.fc27b08b4212ab6239db14f37b0c6c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eacefd7620c8a2e00633e2295ec45f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5d8e01ba3f9ea77653",
+    "url": "/static/css/main.b6ebcc9f.chunk.css"
+  },
+  {
+    "revision": "8f9cdbdbd42d643dcdca",
+    "url": "/static/js/2.b7b1ee97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7b1ee97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5d8e01ba3f9ea77653",
+    "url": "/static/js/main.6f8a2dce.chunk.js"
+  },
+  {
+    "revision": "cf27343b5d386281fb60",
+    "url": "/static/js/runtime-main.1e9c0366.js"
+  }
+]);
